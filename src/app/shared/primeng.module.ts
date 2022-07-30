5b3969478026d891c81538f623e21d5bc5d9e559
kevink01 <klpv733@gmail.com>
2022-07-29 21:10:06 -0400

    Working toolbar in PrimeNG

--- src/app/shared/primeng.module.ts
+++ src/app/shared/primeng.module.ts
@@ -2,7 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
-const MODULES = [ToolbarModule, ButtonModule];
+import { MenubarModule } from 'primeng/menubar';
+import { MenuModule } from 'primeng/menu';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { SidebarModule } from 'primeng/sidebar';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+const MODULES = [
+  CardModule,
+  ToolbarModule,
+  ButtonModule,
+  DividerModule,
+  ScrollPanelModule,
+  MenuModule,
+  MenubarModule,
+  TieredMenuModule,
+  SidebarModule,
+];
 @NgModule({
   declarations: [],
   imports: [CommonModule, MODULES],
